55328b49261b01cb5368e10b24dcbae9c59940ab
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-30 14:10:11 +0200

    position of spaceship can be changed by touching

--- core/src/com/lappard/android/LeopardRun.java
+++ core/src/com/lappard/android/LeopardRun.java
@@ -2,18 +2,34 @@ package com.lappard.android;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Net;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.net.Socket;
+
+import java.io.IOException;
 
 public class LeopardRun extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+    Sprite spaceship;
+    Socket socket;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("spaceship.png");
+        Texture img = new Texture("spaceship.png");
+        spaceship = new Sprite(img, img.getWidth(), img.getHeight());
+        Gdx.input.setInputProcessor(new InputAdapter(){
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                spaceship.setCenter(screenX, Gdx.graphics.getHeight() - screenY);
+                return true;
+            }
+        });
 	}
 
 	@Override
@@ -21,7 +37,7 @@ public class LeopardRun extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+        spaceship.draw(batch);
 		batch.end();
 	}
 }
