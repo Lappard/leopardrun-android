406783452ed32647aceafb27624b7369586a3d14
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-30 13:05:42 +0200

    setup libgdx project

---
+++ android/src/com/lappard/android/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lappard.android.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lappard.android.LeopardRun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LeopardRun(), config);
+	}
+}
