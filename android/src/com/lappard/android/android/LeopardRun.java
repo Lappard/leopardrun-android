b36cba836da2b57d5a8ab5dc887e0743f6265d85
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-05-04 20:33:58 +0200

    tidy up project, removed core project and put everything in android project

---
+++ android/src/com/lappard/android/android/LeopardRun.java
@@ -0,0 +1,42 @@
+package com.lappard.android.android;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class LeopardRun extends ApplicationAdapter {
+
+    SpriteBatch batch;
+    Sprite spaceship;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        Texture img = new Texture("spaceship.png");
+        spaceship = new Sprite(img, img.getWidth(), img.getHeight());
+
+
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                spaceship.setCenter(screenX, Gdx.graphics.getHeight() - screenY);
+                //socket.send("{\"x\":" + screenX + ", \"y\":" + screenY + "}");
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        spaceship.draw(batch);
+        batch.end();
+    }
+}
