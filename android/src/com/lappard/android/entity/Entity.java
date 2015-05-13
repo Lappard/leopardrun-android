5ff7537996ea250330f5d086119db8623345a3de
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-05-13 10:16:35 +0200

    start implementing player

--- android/src/com/lappard/android/entity/Entity.java
+++ android/src/com/lappard/android/entity/Entity.java
@@ -1,5 +1,13 @@
 package com.lappard.android.entity;
 
-public class Entity {
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
+public abstract class Entity {
+
+    public abstract void update();
+
+    public abstract void render();
 
 }
--- android/src/com/lappard/android/entity/Player.java
+++ android/src/com/lappard/android/entity/Player.java
@@ -1,4 +1,50 @@
 package com.lappard.android.entity;
 
-public class Player {
+import android.graphics.Rect;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Player extends Entity{
+
+    private ShapeRenderer shapeRenderer;
+    private Rectangle rect;
+    private Body body;
+
+    public Player(int x, int y, World w){
+        shapeRenderer = new ShapeRenderer();
+        rect = new Rectangle(0,0,10,20);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(rect.getX(), rect.getY());
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(rect.getWidth() / 2f, rect.getHeight() / 2f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 1, 0, 1);
+        shapeRenderer.rect(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
+        shapeRenderer.end();
+    }
 }
