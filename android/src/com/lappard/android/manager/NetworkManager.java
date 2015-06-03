e1f444d35abef9ce365daa47b32e29d069959009
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-06-03 15:01:14 +0200

    refactored network manager

--- android/src/com/lappard/android/manager/NetworkManager.java
+++ android/src/com/lappard/android/manager/NetworkManager.java
@@ -2,17 +2,32 @@ package com.lappard.android.manager;
 
 import android.util.Log;
 
+import com.badlogic.gdx.math.Vector2;
 import com.codebutler.android_websockets.WebSocketClient;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Vector;
 
 public class NetworkManager {
     private static final String TAG_WS = "WebSocket";
     private static final String WEBSOCKET_URL = "ws://jonathanwiemers.de:1337";
 
+    private static final String ACTION_CREATE_LEVEL = "create level";
+
     private WebSocketClient socket;
+    private List<EventListener> listeners;
+
+    public interface EventListener{
+
+        public void onEvent(String Event);
+    }
 
     public NetworkManager(){
+        listeners = new Vector<EventListener>();
+    }
+
+    private void prepareConnection() {
         socket = new WebSocketClient(URI.create(WEBSOCKET_URL),new WebSocketClient.Listener() {
             @Override
             public void onConnect() {
@@ -41,9 +56,9 @@ public class NetworkManager {
                 error.printStackTrace();
             }
         }, null);
-    }
+    };
 
-    public void connect(){
+    public void connect() {
         socket.connect();
     }
 }
