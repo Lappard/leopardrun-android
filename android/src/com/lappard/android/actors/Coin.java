bb441d94498cd47a2769408d760adb7b52dbcb82
Tim Pissarczyk <tim@pissarczyk.com>
2015-07-14 18:41:39 +0200

    Added coin

---
+++ android/src/com/lappard/android/actors/Coin.java
@@ -0,0 +1,40 @@
+package com.lappard.android.actors;
+
+import android.util.Log;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.lappard.android.graphic.AnimatedSprite;
+import com.lappard.android.graphic.AssetManager;
+import com.lappard.android.logic.ScoreManager;
+
+public class Coin extends PhysicsActor {
+    public static float SIZE = 70;
+
+    public Coin(World world, float x, float y) {
+        AnimatedSprite sprite = new AnimatedSprite(AssetManager.getInstance().getTexture(AssetManager.TEXTURE_COIN),6,1,0.1f);
+        this.sprite = sprite;
+        this.sprite.setSize(SIZE,SIZE);
+        this.collisionCategory = Obstacle.COLLISION_CATEGORY;
+        this.collisionMask = Player.COLLISION_CATEGORY;
+        super.initPhysicsAsBox(world,x,y, BodyDef.BodyType.StaticBody);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void onContact(Actor other, Contact contact) {
+        if(other instanceof FireWall){
+            sprite.setColor(0.2f, 0.1f, 0.1f, 1);
+        } else if (other instanceof Player) {
+            this.remove();
+            ScoreManager.getInstance().addCoin();
+        }
+    }
+}
